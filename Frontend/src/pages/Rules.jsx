cc5bbdf59fe8f3654170cb8137c9ec241d48fb91
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-04-07 21:56:14 +0545

    rules page and style add

--- Frontend/src/pages/Rules.jsx
+++ Frontend/src/pages/Rules.jsx
@@ -1,9 +1,149 @@
 import React from 'react'
 import Navbarcomponent from '../components/Navbarcomponent'
-
+import buil3 from '../testimgs/randomb3.jpg'
+import '../css/rules.css'
 const Rules = () => {
   return (
+    <>
     <Navbarcomponent/>
+    <div className='d-flex justify-content-center my-3'>
+        <div className=''>
+          <div className='container'>
+            <h1>The Golden Rules of Harmony</h1>
+          </div>
+                    <div className='container'>
+                    <h4 className='text-center mb-3'>
+            All residents are expected to strictly adhere to the following rules:
+            </h4>
+                    <div className='row mars'>
+            
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Fast Wifi</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+            
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit text-black">
+                            <h5 className="card-title">24/7 Water Supply</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+            
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Laundry</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+            
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Clean Drinking water</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+            
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Electricity</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+            
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+                        
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                        <div className='col-md-4 mb-4'>
+                        <div className="card" style={{width: "18rem", height:"20rem"}}>
+                        <img src={buil3} className="card-img-top" alt={buil3}/>
+                        <div className="card-body cardedit">
+                            <h5 className="card-title">Peaceful Environment</h5>
+                            <p className="card-text text-black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                    </div>
+                    </div>
+
+        </div>
+    </div>
+    </>
+  
   )
 }
 
